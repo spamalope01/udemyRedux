31c9c7cb55ad2be7f8688e90e36d3454fdf8c0aa
Jonathan Daniel <jonathandaniel@Jonathans-MacBook-Pro.local>
2017-12-26 19:02:18 -0800

    got the container to map to state

--- udemyRedux/src/containers/book-list.js
+++ udemyRedux/src/containers/book-list.js
@@ -1,19 +1,32 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+
+class BookList extends Component {
   renderList() {
-    return this.props.books.map((book) => {
-      return(
-        <li key={book.title} className="list-group-item>">{book.title}
-      )
-    })
+    return this.props.books.map(book => {
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      );
+    });
   }
 
-  render(){
+  render() {
     return (
       <ul className="list-group col-sm-4">
         {this.renderList()}
       </ul>
-    )
+    );
   }
 }
+
+
+
+function mapStateToProps(state) {
+
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
